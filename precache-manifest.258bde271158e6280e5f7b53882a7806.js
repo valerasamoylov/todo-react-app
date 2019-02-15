c172b84ec9ab583123cbd236949ef9d4a2969890
Valeriy <valerasam1012@gmail.com>
2019-02-15 23:08:04 +0200

    Updates

---
+++ precache-manifest.258bde271158e6280e5f7b53882a7806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "500e0d85f470a27e025a",
+    "url": "/todo-react-app/static/js/runtime~main.500e0d85.js"
+  },
+  {
+    "revision": "fa47330227c845e58387",
+    "url": "/todo-react-app/static/js/main.fa473302.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/todo-react-app/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "fa47330227c845e58387",
+    "url": "/todo-react-app/static/css/main.332f06d1.chunk.css"
+  },
+  {
+    "revision": "f372f791afe6e0fd229f48b74b251eb7",
+    "url": "/todo-react-app/index.html"
+  }
+];
